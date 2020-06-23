28ed51dddb0f87696693a7b8b9c31e62dcf9fae0
unknown <e10809@pmc.org.tw>
2020-06-23 17:40:16 +0800

    create-update

--- models/Robot.php
+++ models/Robot.php
@@ -23,16 +23,19 @@
         }
         public function create_update(){
             //Create query
-            $query = '
-            UPDATE '.$this->table.' SET  axisstatus = :axisstatus WHERE robotid = :robotid;
+            $query = 'UPDATE '.$this->table.' SET  axisstatus = :axisstatus WHERE robotid = :robotid;
             INSERT INTO  '.$this->table.' (robotid,axisstatus) VALUES (:robotid,:axisstatus) WHERE NOT EXISTS (SELECT 1 FROM '.$this->table.' WHERE robotid = :robotid)' ;
             $stmt = $this->conn->prepare($query);
 
             $this->robotid = htmlspecialchars( strip_tags($this->robotid));
             $this->axisstatus = htmlspecialchars( strip_tags($this->axisstatus));
 
+            echo ( $query);
+
             $stmt->bindParam(':robotid',$this->robotid);
             $stmt->bindParam(':axisstatus',$this->axisstatus);
+
+            echo ($query);
 
             if($stmt->execute()){
                 return true;
