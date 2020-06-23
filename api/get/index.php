6bd7ed4483b8880ae7ea92e473a8a5c37b9af02e
unknown <e10809@pmc.org.tw>
2020-06-23 15:24:06 +0800

    robot commit

--- api/get/index.php
+++ api/get/index.php
@@ -1,3 +1,47 @@
 <?php 
-phpinfo();
+    // header('Access-Control-Allow-Origin: *');
+    // header('Content-Type: application/json');
+    echo('this is bbb');
+    // include_once '../../config/Database.php';
+    // include_once '../../config/Database_pg.php';
+    // include_once '../../models/Robot.php';
+    
+    echo('this is 1');
+
+    // $database = new Database();
+    // $db = $database->connect();
+
+    // echo('this is 2');
+
+    // $Robot = new Robot($db);
+
+    // $result = $Robot->read();
+
+    // $num = $result->rowCount();
+    // echo('this is 3');
+    // //Check if any users
+    // //if($num > 0){
+    //     $posts_arr = array();
+    //     $posts_arr['data'] = array();
+    //     $posts_item =null;
+    //     while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    //         extract($row);
+
+    //         $AXIS = json_decode($AXISSTATUS, true);
+           
+    //         $axisArray = array();
+    //        foreach($AXIS as $a)
+    //        {
+    //             array_push($axisArray,$a);
+    //        } 
+
+    //         $posts_item = array(
+    //             'id' => $ROBOTID,
+    //             'AXISSTATUS' => $axisArray
+    //         );
+    //         array_push($posts_arr['data'],$posts_item);
+    //     }
+
+    //     echo json_encode($posts_item);
+
 ?>
