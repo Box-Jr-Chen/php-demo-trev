e0b5f948bb75e8a5bdce5d37feb7c4047b4f9585
box <box621863@gmail.com>
2020-06-23 22:37:49 +0800

    query change

--- models/Robot.php
+++ models/Robot.php
@@ -23,27 +23,32 @@
         }
         public function create_update(){
             //Create query
-          //   $query = 'UPDATE '.$this->table.' SET  axisstatus = :axisstatus WHERE robotid = :robotid;INSERT INTO  '.$this->table.' (robotid,axisstatus) SELECT :robotid,:axisstatus WHERE NOT EXISTS (SELECT 1 FROM '.$this->table.' WHERE robotid = :robotid)' ;
-             $query = 'CREATE OR REPLACE FUNCTION upsert_robotaxis(key varchar, data  text) RETURNS VOID AS 
-                        $$ 
-                        DECLARE 
-                        BEGIN 
-                            UPDATE '.$this->table.' SET axisstatus = :axisstatus WHERE robotid = :robotid; 
-                            IF NOT FOUND THEN 
-                            INSERT INTO '.$this->table.' values (:robotid,:axisstatus); 
-                            END IF; 
-                        END;
-                        $$ 
-                        LANGUAGE plpgsql;';
-            //$query = 'INSERT INTO  '.$this->table.' (robotid,axisstatus) VALUES (:robotid,:axisstatus) ' ;
+                    //$query = 'INSERT INTO  '.$this->table.' (robotid,axisstatus) VALUES (:robotid,:axisstatus) ' ;
+            //  $query = 'CREATE OR REPLACE FUNCTION upsert_robotaxis(key varchar, data  text) RETURNS VOID AS $$ 
+            //             DECLARE 
+            //             BEGIN 
+            //                 UPDATE '.$this->table.' SET axisstatus = :axisstatus WHERE robotid = :robotid; 
+            //                 IF NOT FOUND THEN 
+            //                 INSERT INTO '.$this->table.' values (:robotid,:axisstatus); 
+            //                 END IF; 
+            //             END;
+            //             $$ 
+            //             LANGUAGE plpgsql;';
+
+
+            $query = 'CREATE OR REPLACE FUNCTION increment(i integer) RETURNS integer AS $$
+            BEGIN
+                    RETURN i + 1;
+            END;
+            $$ LANGUAGE plpgsql;';
 
             $stmt = $this->conn->prepare($query);
 
-            $this->robotid = htmlspecialchars( strip_tags($this->robotid));
-            $this->axisstatus = htmlspecialchars( strip_tags($this->axisstatus));
+            // $this->robotid = htmlspecialchars( strip_tags($this->robotid));
+            // $this->axisstatus = htmlspecialchars( strip_tags($this->axisstatus));
 
-            $stmt->bindParam(':robotid',$this->robotid);
-            $stmt->bindParam(':axisstatus',$this->axisstatus);
+            // $stmt->bindParam(':robotid',$this->robotid);
+            // $stmt->bindParam(':axisstatus',$this->axisstatus);
 
             echo ($query);
 
