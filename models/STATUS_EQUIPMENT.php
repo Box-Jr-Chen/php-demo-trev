49dc84791585eee8a955f8541a96e65668ef1597
unknown <e10809@pmc.org.tw>
2020-06-30 10:10:53 +0800

    fix status 23

--- models/STATUS_EQUIPMENT.php
+++ models/STATUS_EQUIPMENT.php
@@ -36,16 +36,16 @@
 
           //  UPDATE '.$this->table.' SET brand=brand_c,devip=devip_c,subid=subid_c,opid=opid_c,status=status_c,mode=mode_c,linkstatus=linkstatus_c,palletno=palletno_c,mission=mission_c,palletsize=palletsize_c  WHERE equipid = key;
          // INSERT INTO '.$this->table.' (equipid,brand,devip) VALUES (key, brand_c,devip_c,subid_c,opid_c,status_c,mode_c,linkstatus_c,palletno_c,mission_c,palletsize_c);
-                    $query = 'CREATE OR REPLACE FUNCTION merge_db(key varchar,brand_c varchar,devip_c varchar,subid_c varchar,opid_c varchar,status_c varchar,mode_c varchar,linkstatus_c varchar,palletno_c varchar,mission_c varchar) RETURNS VOID AS
+                    $query = 'CREATE OR REPLACE FUNCTION merge_db(key varchar,brand_c varchar,devip_c varchar,subid_c varchar,opid_c varchar,status_c varchar,mode_c varchar,linkstatus_c varchar,palletno_c varchar,mission_c varchar,palletsize_c varchar) RETURNS VOID AS
             $$
             BEGIN
-                    UPDATE '.$this->table.' SET brand=brand_c,devip=devip_c,subid=subid_c,opid=opid_c,status=status_c,mode=mode_c,linkstatus=linkstatus_c,palletno=palletno_c,palletsize=mission_c  WHERE equipid = key;
+                    UPDATE '.$this->table.' SET brand=brand_c,devip=devip_c,subid=subid_c,opid=opid_c,status=status_c,mode=mode_c,linkstatus=linkstatus_c,palletno=palletno_c,mission=mission_c,palletsize=palletsize_c  WHERE equipid = key;
                     IF found THEN
                         RETURN;
                     END IF;
  
                     BEGIN
-                        INSERT INTO '.$this->table.' (equipid,brand,devip,subid,opid,status,mode,linkstatus,palletno,mission,palletsize) VALUES (key, brand_c,devip_c,subid_c,opid_c,status_c,mode_c,linkstatus_c,palletno_c,mission_c,mission_c);
+                        INSERT INTO '.$this->table.' (equipid,brand,devip,subid,opid,status,mode,linkstatus,palletno,mission,palletsize) VALUES (key, brand_c,devip_c,subid_c,opid_c,status_c,mode_c,linkstatus_c,palletno_c,palletsize_c,mission_c);
                         RETURN;
                     EXCEPTION WHEN unique_violation THEN
 
@@ -61,7 +61,7 @@
           //  echo ($query);
 
              if($stmt->execute()){
-                $query2 = 'SELECT merge_db(:equipid,:brand,:devip,:subid,:opid,:status_c,:mode,:linkstatus,:palletno,:palletsize);';
+                $query2 = 'SELECT merge_db(:equipid,:brand,:devip,:subid,:opid,:status_c,:mode,:linkstatus,:palletno,:mission,:palletsize);';
                 $stmt2 = $this->conn->prepare($query2);
 
                 $this->equipid = htmlspecialchars( strip_tags($this->equipid));
@@ -86,7 +86,7 @@
                $stmt2->bindParam(':mode',$this->mode);
                $stmt2->bindParam(':linkstatus',$this->linkstatus);
                $stmt2->bindParam(':palletno',$this->palletno);
-            //    $stmt2->bindParam(':mission',$this->mission);
+               $stmt2->bindParam(':mission',$this->mission);
                $stmt2->bindParam(':palletsize',$this->palletsize);
 
 
